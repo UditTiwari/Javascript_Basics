1d757110e53ffc4e8829bb750537c7d983e88ccc
udit_tiwari <udit.tiwari@quantive.com>
2023-07-30 23:28:22 +0530

    30 july python modules

---
+++ arrays_datatype.js
@@ -0,0 +1,19 @@
+let array1 = [2,4,5,8];
+
+array1.length
+
+array1[array1.length -1]
+
+array1.push(10)
+
+array1.unshift(0) //insert into starting
+
+
+
+array2 = [3,5,7,9]
+
+array2.concat(array2)
+
+
+typeof array2
+// 'object'
---
+++ object_datatype .js
@@ -0,0 +1,7 @@
+student = {"name":"Udit","yearOfBirth":1999,"country":"India"}
+ //student is object and name ,country etc are Property
+
+
+ student["name"]
+
+ student.name
